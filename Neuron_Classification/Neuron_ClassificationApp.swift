b35afb218ac6009025caac9c3bb2f8b742257fc1
Cong Le <longchike@gmail.com>
2025-06-29 04:05:34 -0700

    The setup

---
+++ Neuron_Classification/Neuron_ClassificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neuron_ClassificationApp.swift
+//  Neuron_Classification
+//
+//  Created by Cong Le on 6/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Neuron_ClassificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
